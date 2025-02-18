4e531c6b1472523f7bff2c85f973d2168cb35a9c
Gritsenko Egor <ea@nowhere>
2025-02-18 12:32:10 +0100

    A fancier hello.py : resolved conflicts

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-def Hello(n): print(fHello {n})
+def Hello(n): print(f"Hello {n}")
+
+print("Hello World")
+for i in range(10):
+    s = str(i)
+    for j in range(i - 1, 0, -1):
+        s += ' '
+        s += str(j)
+    print(s)
+
